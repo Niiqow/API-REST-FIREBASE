187b5fff151d10c413219899bd0601f1ae69813c
Nicolas Santiago Almonacid Ojeda <44521679+Niiqow@users.noreply.github.com>
2023-03-27 12:08:23 -0300

    Update index.cjs

--- index.cjs
+++ index.cjs
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 const cors = require('cors');
 
